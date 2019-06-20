e49d386387459bb0f4e0b6887b2740eacb90847d
Russell Kerns <russellkerns@gmail.com>
2019-06-20 10:47:48 -0500

    saved changes to models/index.js

--- models/index.js
+++ models/index.js
@@ -5,13 +5,16 @@ const db = new Sequelize("postgres://localhost:5432/wikistack", {
 
 const Page = db.define("page", {
   title: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   slug: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   content: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT,
+    allowNull: false
   },
   status: {
     type: Sequelize.ENUM("open", "closed")
@@ -20,10 +23,12 @@ const Page = db.define("page", {
 
 const User = db.define("user", {
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   email: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   }
 });
 
